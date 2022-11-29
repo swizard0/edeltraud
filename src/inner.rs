ece1ff7453d321cd7cf66456ba9411b6e0ad4e2a
Alexey Voznyuk <me@swizard.info>
2022-11-29 05:14:49 +0300

    `compare_exchange` -> `compare_exchange_weak`

--- src/inner.rs
+++ src/inner.rs
@@ -64,7 +64,7 @@ impl TouchTag {
 
     fn try_set(&self, prev_tag: u64, new_tag: u64) -> bool {
         self.tag
-            .compare_exchange(
+            .compare_exchange_weak(
                 prev_tag,
                 new_tag,
                 atomic::Ordering::Acquire,
